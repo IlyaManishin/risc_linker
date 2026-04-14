e18eb666ed2e3dc5a2ba455a8f26e2f919f95f23
IlyaManishin <manishin06@gmail.com>
2026-04-14 13:01:01 +0700

    add asm _start, add gp

---
+++ risc_test/linker/start.s
@@ -0,0 +1,27 @@
+	.option nopic
+	.attribute arch, "rv32i2p1"
+	.attribute unaligned_access, 0
+	.attribute stack_align, 16
+	.text
+	.section	.text.start,"ax",@progbits
+	.align	2
+	.globl	_start
+	.type	_start, @function
+_start:
+    lui	gp,%hi(__GLOBAL_POINTER__)
+	addi	gp,gp,%lo(__GLOBAL_POINTER__)
+
+	lui	a5,%hi(__BSS_START__)
+	addi	a5,a5,%lo(__BSS_START__)
+	lui	a4,%hi(__BSS_END__)
+	addi	a4,a4,%lo(__BSS_END__)
+    
+.L2:
+	bltu	a5,a4,.L3
+	call	main
+.L4:
+	j	.L4
+.L3:
+	addi	a5,a5,1
+	sb	zero,-1(a5)
+	j	.L2
--- risc_test/test.c
+++ risc_test/test.c
@@ -1,7 +1,14 @@
-static volatile int t = 7;
-
 int main()
 {
-    volatile int a, b, c;
-    a = b + c + t;
+    unsigned int a = 0xDEADBEEF;
+
+    a = a - ((a >> 1) & 0x55555555);
+    a = (a & 0x33333333) + ((a >> 2) & 0x33333333);
+    a = (a + (a >> 4)) & 0x0F0F0F0F;
+    a = a + (a >> 8);
+    a = a + (a >> 16);
+    a = a & 0x3F;
+
+    int r = (a == 24)? 1 : 0; 
+    return r;
 }
