03582049df6271890ef586fbd55011e6fc434939
IlyaManishin <manishin06@gmail.com>
2026-04-17 18:36:49 +0700

    add stack init, add SRC arg to make

--- risc_test/linker/start.s
+++ risc_test/linker/start.s
@@ -2,12 +2,15 @@
 	.attribute arch, "rv32i2p1"
 	.text
 	.section	.text.start,"ax",@progbits
-	.align	2
+	.align	4
 	.globl	_start
 	.type	_start, @function
 _start:
     lui	gp,%hi(__global_pointer$)
 	addi	gp,gp,%lo(__global_pointer$)
+
+	lui	sp,%hi(__stack_top)
+	addi	sp,sp,%lo(__stack_top)
 
 	lui	a5,%hi(__BSS_START__)
 	addi	a5,a5,%lo(__BSS_START__)
@@ -18,6 +21,7 @@ _start:
 	bltu	a5,a4,.L3
 	call	main
 .L4:
+	nop #remove maybe 
 	j	.L4
 .L3:
 	addi	a5,a5,1
